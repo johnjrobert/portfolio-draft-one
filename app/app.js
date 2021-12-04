856033c8b25420fc4e7bd0ef1a2ce7863ee602f8
johnjrobert <78804682+johnjrobert@users.noreply.github.com>
2021-12-04 13:05:29 -0500

    Updated mobile menu, made animations for hamburger menu

--- app/app.js
+++ app/app.js
@@ -18,6 +18,27 @@ function hamburgerMenuToggle() {
   });
 }
 
+//needs to be redone in jquery.  This is for mobile menu.
+const hamburger = document.querySelector(".menu-icon");
+const navMenu = document.querySelector(".nav-side-bar");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+//for closing the menu
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
 function initListeners() {
   // navClick();
   hamburgerMenuToggle();
